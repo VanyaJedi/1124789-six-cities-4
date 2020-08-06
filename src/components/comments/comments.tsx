fb227876574cd0e755fa63a130b1e5f886e1621f
Ivan <vanya71310@gmail.com>
2020-08-06 20:33:49 +0300

    disable form when submit

--- src/components/comments/comments.tsx
+++ src/components/comments/comments.tsx
@@ -35,6 +35,7 @@ const Comments: React.FunctionComponent<Props> = (props: Props) => {
                   id={input.htmlId}
                   type="radio"
                   checked={input.value === rating}
+                  disabled={isSubmiting}
                 />
                 <label htmlFor={input.htmlId} className="reviews__rating-label form__rating-label" title="perfect">
                   <svg className="form__star-image" width="37" height="33">
@@ -47,7 +48,7 @@ const Comments: React.FunctionComponent<Props> = (props: Props) => {
         }
 
       </div>
-      <textarea onChange={onInputChange} value={review} className="reviews__textarea form__textarea" id="review" name="review" placeholder="Tell how was your stay, what you like and what can be improved"></textarea>
+      <textarea disabled={isSubmiting} onChange={onInputChange} value={review} className="reviews__textarea form__textarea" id="review" name="review" placeholder="Tell how was your stay, what you like and what can be improved"></textarea>
       <div className="reviews__button-wrapper">
         <p className="reviews__help">
           To submit review please make sure to set <span className="reviews__star">rating</span> and describe your stay with at least <b className="reviews__text-amount">50 characters</b>.
